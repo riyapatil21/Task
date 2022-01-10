e36e2d896fab2e508e6c0590d4d49eac59d9afec
riyapatil21 <97428959+riyapatil21@users.noreply.github.com>
2022-01-10 00:23:26 -0800

    Add files via upload

--- Task/LatitudeTask.py
+++ Task/LatitudeTask.py
@@ -1,21 +1,21 @@
-def Pushzero(num_list, n):       
+def Pushzero(arr, n):       
     count = 0 
     for i in range(n):
-        if num_list[i] != 0:
-            num_list[count] = num_list[i]
+        if arr[i] != 0:
+            arr[count] = arr[i]
             count+=1
     while count < n:
-        num_list[count] = 0
+        arr[count] = 0
         count += 1
 #Driver code
-num_list = [12,0,16,0,0,0,20,70]
-n = len(num_list)
-Pushzero(num_list, n)
-num_list = [90,0,20,0,0,80,10,0]
-n1 = len(num_list)
-Pushzero(num_list, n1)
+num_list1 = [12,0,16,0,0,0,20,70]
+n = len(num_list1)
+Pushzero(num_list1, n)
+num_list2 = [90,0,20,0,0,80,10,0]
+n = len(num_list2)
+Pushzero(num_list2, n)
 print("Array after pushing all zeros to end of array:")
-print(num_list)
-print(num_list)
+print(num_list1)
+print(num_list2)
 
 
